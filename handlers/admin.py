09084342f721dd5d9c7094416ad471ae96a784b8
eromanv <e.roman.v@ya.ru>
2023-05-31 05:58:15 -0300

    added three cabinets

--- handlers/admin.py
+++ handlers/admin.py
@@ -8,4 +8,36 @@ class FSMAdmin(StatesGroup):
     recipe = State()
 
 @dp.message_handler(commands='Загрузить', state=None)
-async def cm_start(message : types.Message):
+async def cm_start(message: types.Message):
+    await FSMAdmin.name.set()
+    await message.reply('Напиши имя блюда')
+
+@dp.message_handler(content_types=['text'], state=FSMAdmin.name)
+async def load_name(message: types.Message, state=FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Введи количество калорий в формате К_БЖУ')
+
+@dp.message_handler(content_types=['text'], state=FSMAdmin.calories)
+async def load_calories(message: types.Message, state=FSMContext):
+    async with state.proxy() as data:
+        data['calories'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши ингридиенты и их количество')
+
+@dp.message_handler(content_types=['text'], state=FSMAdmin.ingredients)
+async def load_ingredients(message: types.Message, state=FSMContext):
+    async with state.proxy() as data:
+        data['ingredients'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Напиши рецепт приготовления')
+
+@dp.message_handler(content_types=['text'], state=FSMAdmin.recipe)
+async def load_recipe(message: types.Message, state=FSMContext):
+    async with state.proxy() as data:
+        data['recipe'] = message.text
+
+    
+    
+    await state.finish()
